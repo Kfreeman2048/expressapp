c8b4c3fcd32ac000f007cef4d80deae117b10ce4
Kfreeman2048 <kfreeman2048@gmail.com>
2023-04-20 17:30:05 -0500

    first commit setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.listen(PORT, () => {
+    console.log(`Server runnin gon port ${PORT}`);
+});
